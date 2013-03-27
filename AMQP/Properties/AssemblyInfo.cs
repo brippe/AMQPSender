de6511e69453bdf5e1b6fbaff1c6746ba8e495a1
Brad Rippe <brad@ripware.com>
2013-03-27 16:36:40 -0700

    Update library info

--- AMQP/Properties/AssemblyInfo.cs
+++ AMQP/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AMQP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Allow you to send messages via Powershell")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AMQP")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
