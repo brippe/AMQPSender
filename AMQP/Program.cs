a1d80e2038ab9446de3071bbf779a60abad7f5d1
Brad Rippe <brad@ripware.com>
2013-03-27 16:33:04 -0700

    Update program

--- AMQP/Program.cs
+++ AMQP/Program.cs
@@ -7,18 +7,7 @@ namespace AMQP
 {
     public class CampusAMQPSender
     {
-        //private static string server = ConfigurationManager.AppSettings["host"];
-        //private static string exchange = ConfigurationManager.AppSettings["exchange"];
-        //static string exchangeType = "topic";
-        //static string routingKey = "fc"; // cc, fc, or sce        
-        // can't get appsettings to work in Win 2008 -- had to move info to powershell script
         
-        /*static void Main(string[] args)
-        {
-            CampusAMQPSender.publishMessage("fc", "Brad sent message from C#");
-            Console.Out.WriteLine("Sent message to amqp server");            
-        }*/
-
         public static void publishMessage(string host, string exchange, string routingKey, string msg)
         {
             ConnectionFactory cf = new ConnectionFactory();            
