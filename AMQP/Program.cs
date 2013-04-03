755da6bfc2a64f46d4785d0055c4cb808838bc61
Brad Rippe <brad@ripware.com>
2013-04-03 15:28:12 -0700

    Updated with logging and use of AMQP.dll.config properties

--- AMQP/Program.cs
+++ AMQP/Program.cs
@@ -2,44 +2,64 @@
 using System.Text;
 using System.Configuration;
 using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
 using RabbitMQ.Client;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace AMQP
 {
     public class CampusAMQPSender
     {
-        // sso-shibboleth.nocccd.edu 10.200.7.236 - amqps://admin:<pass>@sso-shibboleth:5671
-        static void Main(string[] args)
-        {
-            CampusAMQPSender.publishMessage("amqps://admin:Pass4Rabbit$@sso-shibboleth:5671", "nocccd_x", "fc", 
-                "Brad sent message from C#", true);
-            Console.Out.WriteLine("Sent message to amqp server"); 
+        private static string host;
+        private static string exchange;
+        private static string clientKey;
+        private static string passPhrase;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // In visual studio; app looks for App.config (exe file); as dll app look for AMQP.dll.config
+        static CampusAMQPSender() {            
+            log.Debug("CampusAMQPSender");
+            Assembly ea = System.Reflection.Assembly.GetExecutingAssembly();            
+            Console.WriteLine(ea.Location);
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ea.Location);            
+            host = config.AppSettings.Settings["host"].Value.ToString();            
+            log.Debug("host: " + host);            
+            exchange = config.AppSettings.Settings["exchange"].Value.ToString();
+            log.Debug("exchange: " + exchange);            
+            clientKey = config.AppSettings.Settings["clientKey"].Value.ToString();
+            log.Debug("clientKey: " + clientKey);            
+            passPhrase = config.AppSettings.Settings["passPhrase"].Value.ToString();
+            log.Debug("passPhrase: " + passPhrase);            
         }
 
-        public static void publishMessage(string host, string exchange, string routingKey, string msg, bool persist)
+        public static void publishMessage(string routingKey, string msg, bool persist)
         {
             try
             {
                 ConnectionFactory cf = new ConnectionFactory();
                 cf.Uri = host;     // this should be in some config file
-                cf.Ssl.CertPath = "C:\\Users\\brippe\\Documents\\Visual Studio 2010\\Projects\\AMQP\\AMQP\\bin\\x64\\keycert.p12";
-                cf.Ssl.CertPassphrase = "Pass4Cert";
+                cf.Ssl.CertPath = clientKey; //"C:\\Users\\brippe\\Documents\\Visual Studio 2010\\Projects\\AMQP\\AMQP\\bin\\x64\\keycert.p12";
+                cf.Ssl.CertPassphrase = passPhrase;
                 cf.Ssl.Enabled = true;
 
                 using (IConnection conn = cf.CreateConnection())
                 {
+                    log.Debug("Connection established");
                     using (IModel ch = conn.CreateModel())
                     {
                         IBasicProperties bProps = ch.CreateBasicProperties();
                         bProps.SetPersistent(persist);
+                        log.Debug("Routing to binding " + routingKey);
+                        log.Debug("Persist message: " + persist);
                         ch.BasicPublish(exchange, routingKey, bProps, Encoding.UTF8.GetBytes(msg));
+                        log.Debug(msg + " sent to queue");
                     }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                log.Error(ex.ToString());
             }
         }
     }
